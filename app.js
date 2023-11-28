0688b293a1be668eee36067858f7365807d57141
Hui Zou <zou.hui@northeastern.edu>
2023-11-27 23:01:09 -0800

    app.js

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ import HelloRoutes from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // 设置服务器选择超时时间（毫秒）
+    socketTimeoutMS: 45000, // 设置 socket 超时时间（毫秒）
+});
+
 
 import UserRoutes from "./users/routes.js";
 import CourseRoutes from "./courses/routes.js";
@@ -16,7 +24,8 @@ import AssignmentRoutes from "./assignments/routes.js";
 const app = express();
 const corsOptions = {
     credentials: true, 
-    origin: 'http://localhost:3000',
+    //origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL
   };
 app.use(cors(corsOptions)); 
 
@@ -25,6 +34,13 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+  sameSite: "none",
+  secure: true,
+  };
+}
 app.use(
   session(sessionOptions)
 );
