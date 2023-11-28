b0268c33e5e318fb373d656f23c65f9a584afb6c
Hui Zou <zou.hui@northeastern.edu>
2023-11-27 21:26:13 -0800

    a6

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 //const express = require('express');
 import "dotenv/config";
+import session from "express-session";
 import express from "express";
 import HelloRoutes from "./hello.js"; 
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
+import UserRoutes from "./users/routes.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 
 const app = express();
-app.use(cors());   
+const corsOptions = {
+    credentials: true, 
+    origin: 'http://localhost:3000',
+  };
+app.use(cors(corsOptions)); 
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(
+  session(sessionOptions)
+);
 app.use(express.json());
 
 Lab5(app);
@@ -17,5 +35,7 @@ HelloRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
-app.listen(process.env.PORT || 4000);
+//app.listen(process.env.PORT || 4000);
+app.listen(4000);
---
+++ users/routes.js
@@ -0,0 +1,132 @@
+import * as dao from "./dao.js";
+
+//let currentUser = null;
+
+function UserRoutes(app) {
+
+  // const signin = async (req, res) => {
+  //   const { username, password } = req.body;
+  // const currentUser = await dao.findUserByCredentials(username, password);
+  // req.session['currentUser'] = currentUser;
+  // res.json(currentUser);
+  // };
+
+  const signin = async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const currentUser = await dao.findUserByCredentials(username, password);
+      if (currentUser) {
+        req.session['currentUser'] = currentUser;
+        res.json(currentUser);
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+
+  // const account = async (req, res) => {
+  //   try{
+  //     const currentUser = req.session['currentUser'];
+  //     if(!currentUser){
+  //       res.status(403).json({ error: 'Please sign in.' });
+  //     }
+  //     res.json(currentUser);
+  //   } catch(error){
+  //     res.status(500).json({ error: 'An error occurred while getting account.' });
+  //   }
+  // };
+  const account = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(403);
+      return;
+    }
+    res.json(currentUser);
+  };
+
+
+  const updateUser = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const status = await dao.updateUser(userId, req.body);
+      const currentUser = await dao.findUserById(userId);
+      // 可以选择返回更新后的用户信息 res.json(currentUser);
+      req.session['currentUser'] = currentUser;  // 或者如果只想返回操作状态：
+      res.json(status);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  const findAllUsers = async (req, res) => {
+    try {
+      const users = await dao.findAllUsers();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while find all users.' });
+    }
+  };
+
+  const createUser = async (req, res) => {
+    try {
+      const user = await dao.createUser(req.body);
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while creating the user.' });
+    }
+  };
+
+  const findUserById = async (req, res) => {
+    try {
+      const user = await dao.findUserById(req.params.userId);
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while find the user by ID.' });
+    }
+  };
+
+  const deleteUser = async (req, res) => {
+    try {
+      const status = await dao.deleteUser(req.params.userId);
+      res.json(status);
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while deleting the user.' });
+    }
+  };
+
+  const signup = async (req, res) => {
+    const user = await dao.findUserByUsername(req.body.username);
+    if (user) {
+      res.status(400).json(
+        { message: "Username already taken" });
+    }
+    const currentUser = await dao.createUser(req.body);
+    req.session['currentUser'] = currentUser;
+    res.json(currentUser)
+  };
+
+  const signout = (req, res) => {
+    try {
+      req.session.destroy();
+      res.status(200).json({ message: 'Successfully signed out.' });
+    } catch (error) {
+      console.error('Sign out error:', error);
+      res.status(500).json({ error: 'An error occurred during sign out.' });
+    }
+  };
+
+  app.post("/api/users", createUser);
+  app.get("/api/users", findAllUsers);
+  app.get("/api/users/:userId", findUserById);
+  app.put("/api/users/:userId", updateUser);
+  app.delete("/api/users/:userId", deleteUser);
+  app.post("/api/users/signup", signup);
+  app.post("/api/users/signin", signin);
+  app.post("/api/users/signout", signout);
+  app.post("/api/users/account", account);
+}
+
+export default UserRoutes;
