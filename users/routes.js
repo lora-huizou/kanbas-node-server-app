e972aafd2ab4a99fd7a31720ffcc9b713fea8a1e
Hui Zou <zou.hui@northeastern.edu>
2023-11-30 15:28:20 -0800

    change route.js app.post

--- users/routes.js
+++ users/routes.js
@@ -123,7 +123,7 @@ function UserRoutes(app) {
   app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
   app.post("/api/users/signup", signup);
-  app.post("/api/users/signin", signin);
+  app.post("/users/signin", signin);
   app.post("/api/users/signout", signout);
   app.post("/api/users/account", account);
 }
