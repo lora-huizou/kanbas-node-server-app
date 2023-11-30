a824a0985429864af40a44af9cb807df9ab452ad
Hui Zou <zou.hui@northeastern.edu>
2023-11-30 14:10:23 -0800

    change env

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
 
-
 const app = express();
 
 // const corsOptions = {
